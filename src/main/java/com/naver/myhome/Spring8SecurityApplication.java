3c70d14a741a328c2f034b2a72f3980bcbcb1351
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-04-25 17:51:02 +0900

    Study: Spring8 Security 교육 - 1

---
+++ src/main/java/com/naver/myhome/Spring8SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.naver.myhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring8SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring8SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naver/myhome/Spring8SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naver.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring8SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
